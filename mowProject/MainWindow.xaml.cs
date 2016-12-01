26f9675dd9ec9474b581de8bfe068752c8fea0c6
Pichoule <florian.gojon@epsi.fr>
2016-12-01 14:59:31 +0100

    Initialise wpf

--- mowProject/MainWindow.xaml.cs
+++ mowProject/MainWindow.xaml.cs
@@ -16,8 +16,6 @@ using System.Drawing;
 
 namespace mowProject
 {
-
-
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
@@ -25,18 +23,39 @@ namespace mowProject
     {
         public MainWindow()
         {
-            
-
-
-            // TODO: Comprendre comment coder le choix du nom selon joueur humain ou IA
-            // TODO: Terminer les classes et générer le code une bonne fois pour toute
-
-            // TODO: Théo -> fonction génération aléatoire d'un numéro et sélection du nom pour l'IA dans la liste des noms
-            // new List<string>() { "George", "Jaqueline", "Andreas", "Jacob" };
-            // exemple: List<string> ListeNom = File.ReadAllLines(@"C:\");
-
-
             InitializeComponent();
+            var uriSource = new Uri(@"C:\Users\Admin\Desktop\EPSI\C#\mow\Vaches\Vache_4.png");
+            main1.Source = new BitmapImage(uriSource);
+            var uriSource2 = new Uri(@"C:\Users\Admin\Desktop\EPSI\C#\mow\Vaches\Vache_9.png");
+            main2.Source = new BitmapImage(uriSource2);
+            var uriSource3 = new Uri(@"C:\Users\Admin\Desktop\EPSI\C#\mow\Vaches\Vache_3.png");
+            main3.Source = new BitmapImage(uriSource3);
+            var uriSource4 = new Uri(@"C:\Users\Admin\Desktop\EPSI\C#\mow\Vaches\Vache_13.png");
+            main4.Source = new BitmapImage(uriSource4);
+            var uriSource5 = new Uri(@"C:\Users\Admin\Desktop\EPSI\C#\mow\Vaches\Vache_5.png");
+            main5.Source = new BitmapImage(uriSource5);
         }
-    }
-}
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Hi team !");
+        }
+
+        private void button_Click_1(object sender, RoutedEventArgs e)
+        {
+            if (Place3.Source == null)
+            {
+                var uriSource = new Uri(@"C:\Users\Admin\Desktop\EPSI\C#\mow\Vaches\Vache_9.png");
+                Place3.Source = new BitmapImage(uriSource);
+
+            }
+            else
+            {
+                var uriSource2 = new Uri(@"C:\Users\Admin\Desktop\EPSI\C#\mow\Vaches\Vache_4.png");
+                Place1.Source = new BitmapImage(uriSource2);
+            }
+
+        }
+
+    } // Fin class MainWindow : Window
+} // Fin namespace mowProject
